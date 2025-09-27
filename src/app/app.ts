0530905fecabb65aa9d41a1f2e9379f351b92abc
Roberto Ayala <roberth.ayala95@gmail.com>
2025-09-27 00:19:22 -0600

    dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  
+
 }
---
+++ src/app/layout/page/dashboard/dashboard.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatToolbarModule} from '@angular/material/toolbar'
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styles: ``
+})
+export class Dashboard {
+
+}
