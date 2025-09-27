4823fe85f86a892a78ece70b0db262e8fb1868c0
Roberto Ayala <roberth.ayala95@gmail.com>
2025-09-27 11:00:03 -0600

    Agregando fix para sidenav

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -6,6 +6,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Title } from '@angular/platform-browser'; 
+import { Router } from '@angular/router';
+import Swal, { SweetAlertResult } from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
